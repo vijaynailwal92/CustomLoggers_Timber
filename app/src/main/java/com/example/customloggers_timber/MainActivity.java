79719b5a19f5fe304602532496ba2fd3c189625e
vijaynailwal1992 <vijaynailwal92@gmail.com>
2019-03-04 22:27:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/customloggers_timber/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.customloggers_timber;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import timber.log.Timber;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Timber.e("check");
+
+
+    }
+}
